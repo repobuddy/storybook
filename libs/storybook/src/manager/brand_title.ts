5c2cae7f69c7c4c6a1a11ca55ff3383c93071cfe
unional <homawong@gmail.com>
2025-05-03 23:09:47 -0700

    docs: add doc

--- libs/storybook/src/manager/brand_title.ts
+++ libs/storybook/src/manager/brand_title.ts
@@ -11,6 +11,28 @@ export interface BrandTitleOptions {
 	icon?: string | undefined
 }
 
+/**
+ * Creates a brand title element for the Storybook manager UI.
+ *
+ * @param options - The options for customizing the brand title
+ * @param options.title - The title text or HTML to display
+ * @param options.icon - Optional icon HTML to display before the title
+ * @returns An HTML string containing the brand title element
+ *
+ * @example
+ *
+ * ```ts
+ * import { brandTitle } from '@repobuddy/storybook'
+ * import { addons } from '@storybook/manager-api'
+ *
+ * addons.setConfig({
+ *   brandTitle: brandTitle({
+ *     title: 'My Storybook',
+ *     icon: '<img src="logo.png" alt="Logo" width="24" height="24">'
+ *   })
+ * })
+ * ```
+ */
 export function brandTitle(options: BrandTitleOptions) {
 	return `<span style="display: flex; align-items: center; gap: 2px;">
 		${options.icon ?? ''}
