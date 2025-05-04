4c46b229656c061ce7b59fc6c17c3979729de57c
unional <homawong@gmail.com>
2025-05-03 23:05:09 -0700

    chore: remove manageHead, doesn't work

--- libs/storybook/.storybook/main.ts
+++ libs/storybook/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
-const config: StorybookConfig = {
+export default {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.tsx'],
 	addons: [
 		'@storybook/addon-essentials',
@@ -11,10 +11,5 @@ const config: StorybookConfig = {
 	framework: {
 		name: '@storybook/react-vite',
 		options: {}
-	},
-	managerHead: (head) => `
-		${head ? head.replace(/<link\s+rel=["']icon["'][^>]*>/gi, '') : ''}
-		<link rel="icon" type="image/svg+xml" href="https://avatars.githubusercontent.com/u/97370623?s=400&u=9698dbb5f8f3590a3d7f50e7b5ce6f3db72fcc35&v=4" />
-	`
-}
-export default config
+	}
+} satisfies StorybookConfig
