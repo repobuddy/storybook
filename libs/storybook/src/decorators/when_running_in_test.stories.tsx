2f3d9f98ce16a53cf41d7c088d861efebe8f2cb1
Homa Wong <hwong@paloaltonetworks.com>
2025-05-08 11:04:24 -0700

    feat: add whenRunningInTest

---
+++ libs/storybook/src/decorators/when_running_in_test.stories.tsx
@@ -0,0 +1,56 @@
+import { isRunningInTest } from '@repobuddy/vitest'
+import type { Meta, StoryObj } from '@storybook/react'
+import { expect } from '@storybook/test'
+import { showDocSource, whenRunningInTest } from '../index.ts'
+import { ctx } from './when_running_in_text.ctx.ts'
+
+export default {
+	title: 'testing/whenRunningInTest',
+	tags: ['new', 'version:1.0.0'],
+	beforeEach: () => {
+		ctx.isRunningInTest = () => true
+	},
+	decorators: [showDocSource()],
+	render: () => <></>,
+	experimental_afterEach: () => {
+		ctx.isRunningInTest = isRunningInTest
+	}
+} satisfies Meta
+
+export const BasicUsage: StoryObj = {
+	decorators: [
+		whenRunningInTest((Story) => (
+			<div>
+				<div>Decorator attached</div>
+				<Story />
+			</div>
+		))
+	]
+}
+
+export const AcceptHandler: StoryObj = {
+	loaders: [() => ({ state: { counter: 0 } })],
+	decorators: [
+		whenRunningInTest((_, { loaded: { state } }) => {
+			state.counter = 1
+		})
+	],
+	render: () => <div>When passing in a handler (which returns nothing), the story will be rendered</div>,
+	play: async ({ canvas, loaded: { state } }) => {
+		await expect(state.counter).toBe(1)
+		await expect(canvas.getByText(/When passing in a handler/)).toBeInTheDocument()
+	}
+}
+
+export const SkipDecorator: StoryObj = {
+	tags: ['unit'],
+	beforeEach: () => {
+		ctx.isRunningInTest = () => false
+	},
+	decorators: [
+		whenRunningInTest(() => {
+			throw new Error('should not run')
+		})
+	],
+	render: () => <>should not run decorator when not in test</>
+}
---
+++ libs/storybook/src/decorators/when_running_in_text.ctx.ts
@@ -0,0 +1,5 @@
+import { isRunningInTest } from '@repobuddy/vitest'
+
+export const ctx = {
+	isRunningInTest
+}
