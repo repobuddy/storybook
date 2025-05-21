1a509e539c5a27435762fe1464ee2391d86278a1
Homa Wong <hwong@paloaltonetworks.com>
2025-05-21 05:29:22 -0700

    feat: Add `createDarkModeDocsContainer`

--- libs/storybook/.storybook/preview.tsx
+++ libs/storybook/.storybook/preview.tsx
@@ -1,17 +1,27 @@
 import type { Preview } from '@storybook/react'
-import { defineParameters } from '../src/index.ts'
+import type { StorybookBuiltInParams } from '../src/index.ts'
+import { createDarkModeDocsContainer } from '../src/react/index.ts'
 
 import '../tailwind.css'
 
 const preview: Preview = {
-	parameters: defineParameters({
+	parameters: {
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
+		},
+		docs: {
+			container: createDarkModeDocsContainer()
+		},
+		options: {
+			storySort: {
+				method: 'alphabetical',
+				order: ['decorators', 'parameters', 'manager', 'testing', '*']
+			}
 		}
-	})
+	} satisfies StorybookBuiltInParams
 }
 
 export default preview
---
+++ libs/storybook/src/react/components/dark_mode_docs_container.tsx
@@ -0,0 +1,43 @@
+import { DocsContainer, type DocsContextProps } from '@storybook/blocks'
+import { type ThemeVars, themes } from '@storybook/theming'
+import { type PropsWithChildren, useEffect, useState } from 'react'
+import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode'
+
+/**
+ * Creates a `DocsContainer` for `storybook` that works with `storybook-dark-mode`.
+ *
+ * @see https://github.com/hipstersmoothie/storybook-dark-mode/issues/282
+ *
+ * @example
+ * ```ts
+ * // .storybook/preview.tsx
+ * const preview: Preview = {
+ *   parameters: {
+ *     docs: {
+ *       container: createDarkModeDocsContainer()
+ *     }
+ *   }
+ * }
+ * ```
+ */
+export function createDarkModeDocsContainer(
+	customThemes: {
+		light: ThemeVars
+		dark: ThemeVars
+	} = themes
+) {
+	return function DarkModeDocsContainer(props: PropsWithChildren<{ context: DocsContextProps }>) {
+		const [isDark, setDark] = useState(true)
+
+		useEffect(() => {
+			props.context.channel.on(DARK_MODE_EVENT_NAME, setDark)
+
+			return () => props.context.channel.removeListener(DARK_MODE_EVENT_NAME, setDark)
+		}, [props.context.channel])
+		return (
+			<DocsContainer {...props} theme={isDark ? customThemes.dark : customThemes.light}>
+				{props.children}
+			</DocsContainer>
+		)
+	}
+}
