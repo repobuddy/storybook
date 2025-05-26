315ebffdef867a322cc4b2355c19ad51b8018244
unional <homawong@gmail.com>
2025-05-26 00:36:53 -0700

    chore: update storybook-addon-vis usage

--- libs/storybook/vitest.config.browser.ts
+++ libs/storybook/vitest.config.browser.ts
@@ -1,13 +1,22 @@
-import { join } from 'node:path'
 import { browserTestPreset } from '@repobuddy/vitest/config'
 import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin'
+import { join } from 'node:path'
 import { storybookVis } from 'storybook-addon-vis/vitest-plugin'
 import { defineProject } from 'vitest/config'
 
 export default defineProject({
 	plugins: [storybookTest({ configDir: join(import.meta.dirname, '.storybook') }), storybookVis(), browserTestPreset()],
 	optimizeDeps: {
-		include: ['react/jsx-dev-runtime']
+		include: [
+			'react/jsx-dev-runtime',
+			'@storybook/react',
+			'storybook-addon-vis/vitest-setup',
+			'@storybook/blocks',
+			'@storybook/theming',
+			'react',
+			'storybook-dark-mode',
+			'@just-web/css'
+		]
 	},
 	test: {
 		name: 'sb:browser',
