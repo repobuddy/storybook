1845c533f99f7969bd402f5156919d50c646d9fc
unional <homawong@gmail.com>
2025-05-31 16:57:53 -0700

    chore: disable storybook-addon-vis
    before it supports sb9

--- libs/storybook/vitest.config.browser.ts
+++ libs/storybook/vitest.config.browser.ts
@@ -1,16 +1,19 @@
 import { browserTestPreset } from '@repobuddy/vitest/config'
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 import { join } from 'node:path'
-import { storybookVis } from 'storybook-addon-vis/vitest-plugin'
 import { defineProject } from 'vitest/config'
 
 export default defineProject({
-	plugins: [storybookTest({ configDir: join(import.meta.dirname, '.storybook') }), storybookVis(), browserTestPreset()],
+	plugins: [
+		storybookTest({ configDir: join(import.meta.dirname, '.storybook') }),
+		// storybookVis(),
+		browserTestPreset()
+	],
 	optimizeDeps: {
 		include: [
 			'react/jsx-dev-runtime',
 			'@storybook/react-vite',
-			'storybook-addon-vis/vitest-setup',
+			// 'storybook-addon-vis/vitest-setup',
 			'@storybook/addon-docs/blocks',
 			'storybook/theming',
 			'react',
