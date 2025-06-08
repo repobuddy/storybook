89abcb37163a642fab76b5728fef64abd7a82829
unional <homawong@gmail.com>
2025-06-08 11:55:02 -0700

    chore: use unbuild to build cjs and esm

---
+++ libs/storybook/build.config.ts
@@ -0,0 +1,9 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+	entries: [
+		{ input: './src/', format: 'cjs', outDir: './cjs' },
+		{ input: './src/', format: 'esm', ext: 'js', outDir: './esm' }
+	],
+	declaration: true
+})
