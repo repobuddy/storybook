25d4358fd879f4e1078bc7660b1ca5c687695582
unional <homawong@gmail.com>
2025-06-11 00:25:48 -0700

    feat: hide variant bar when not used

--- libs/storybook/src/variants/components/variant_bar.stories.tsx
+++ libs/storybook/src/variants/components/variant_bar.stories.tsx
@@ -1,19 +1,51 @@
 import type { Meta, StoryObj } from '#repobuddy/storybook/internal'
 import React from 'react'
-import { ctx } from './variant_bar.ctx.js'
+import { expect } from 'storybook/test'
 import { VariantBar } from './variant_bar.js'
 
 export default {
 	title: 'variants/components/VariantBar'
 } satisfies Meta
 
-export const Hidden: StoryObj = {
-	beforeEach: () => {
-		ctx.addons.getConfig = () => ({})
+export const HiddenWithoutParameters: StoryObj = {
+	loaders: [
+		() => ({
+			api: { getCurrentStoryData: () => ({ parameters: {} }) },
+			addons: { getConfig: () => ({}) }
+		})
+	],
+	render: (_, { loaded: { api, addons } }) => (
+		<div>
+			<div>The VariantBar should be hidden when there are no `variant` parameters</div>
+			<VariantBar api={api} addons={addons} data-testid="variant-bar" />
+		</div>
+	),
+	play: async ({ canvas }) => {
+		const variantBar = canvas.queryByTestId('variant-bar')
+		await expect(variantBar).not.toBeInTheDocument()
+	}
+}
+
+export const WithDefaultConfig: StoryObj = {
+	parameters: {
+		variants: {
+			colorTheme: {
+				strategy: {
+					type: 'className',
+					selector: 'html'
+				},
+				variants: {
+					light: 'light',
+					dark: 'dark'
+				},
+				current: 'light'
+			}
+		}
 	},
-	afterEach: () => {
-		ctx.reset()
-	},
-	loaders: [() => ({ api: { getCurrentStoryData: () => ({ parameters: { colorTheme: 'light' } }) } })],
-	render: (_, { loaded: { api } }) => <VariantBar api={api} />
+	loaders: [
+		() => ({
+			api: { getCurrentStoryData: () => ({ parameters: WithDefaultConfig.parameters }) }
+		})
+	],
+	render: (_, { loaded: { api, addons } }) => <VariantBar api={api} addons={addons} data-testid="variant-bar" />
 }
--- libs/storybook/src/variants/components/variant_bar.tsx
+++ libs/storybook/src/variants/components/variant_bar.tsx
@@ -1,11 +1,21 @@
+import type { DataAttributeProps } from '@just-web/types'
 import React from 'react'
-import type { API } from 'storybook/manager-api'
-import { ctx } from './variant_bar.ctx'
+import type { API, AddonStore } from 'storybook/manager-api'
+import { addons as defaultAddons } from 'storybook/manager-api'
+import { PARAM_KEY } from '../constants.js'
 
-export function VariantBar({ api }: { api: API }) {
-	const config = ctx.addons.getConfig()
+type VariantBarProps = {
+	api: API
+	addons?: AddonStore
+} & DataAttributeProps
+
+export function VariantBar(props: VariantBarProps) {
+	const { api, addons = defaultAddons, ...rest } = props
+	const config = addons.getConfig()
 	const storyData = api.getCurrentStoryData()
 	console.info('variant config', config)
 	console.info('story data', storyData)
-	return <div>VariantBar</div>
+	if (!storyData.parameters?.[PARAM_KEY]) return null
+
+	return <div {...rest}>VariantBar</div>
 }
