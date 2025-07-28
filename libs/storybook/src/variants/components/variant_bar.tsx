b8a9233d1e9c6b4de695c5f0085379d5110b5313
unional <homawong@gmail.com>
2025-07-27 17:14:42 -0700

    chore: show different strategy button

--- libs/storybook/src/variants/components/variant_bar.tsx
+++ libs/storybook/src/variants/components/variant_bar.tsx
@@ -2,7 +2,8 @@ import type { DataAttributeProps } from '@just-web/types'
 import React from 'react'
 import type { API, AddonStore } from 'storybook/manager-api'
 import { addons as defaultAddons } from 'storybook/manager-api'
-import { PARAM_KEY } from '../constants.js'
+import { CONFIG_KEY, PARAM_KEY } from '../constants.js'
+import type { VariantConfigOptions, VariantOptions } from '../types.js'
 
 type VariantBarProps = {
 	api: API
@@ -13,9 +14,35 @@ export function VariantBar(props: VariantBarProps) {
 	const { api, addons = defaultAddons, ...rest } = props
 	const config = addons.getConfig()
 	const storyData = api.getCurrentStoryData()
-	console.info('variant config', config)
-	console.info('story data', storyData)
-	if (!storyData.parameters?.[PARAM_KEY]) return null
+	const variantConfig = config[CONFIG_KEY] as VariantConfigOptions[]
+	const variantParam = storyData?.parameters?.[PARAM_KEY] as Record<string, VariantOptions>
 
-	return <div {...rest}>VariantBar</div>
+	if (!variantParam) return null
+
+	console.info('variant config', variantConfig)
+	const variants = Object.entries(variantParam).map(([key, value]) => {
+		const config = variantConfig?.find((v) => v.key === key)
+		return {
+			key,
+			config,
+			value
+		}
+	})
+
+	return (
+		<div {...rest}>
+			{variants.map((v) => {
+				if (v.value.strategy.type === 'className') {
+					return <div>class</div>
+				}
+				if (v.value.strategy.type === 'dataAttribute') {
+					return <div>data-attribute</div>
+				}
+				if (v.value.strategy.type === 'provider') {
+					return <div>provider</div>
+				}
+				return null
+			})}
+		</div>
+	)
 }
