7fe4a9b4b4ba84f1b890bfb5ae2a6cde2638b272
unional <homawong@gmail.com>
2025-11-16 13:39:58 -0800

    feat: provide default variants config

--- libs/storybook/src/variants/manager.tsx
+++ libs/storybook/src/variants/manager.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { addons, types } from 'storybook/manager-api'
 import { VariantBar } from './components/variant_bar.js'
 import { ADDON_ID, TOOL_ID } from './constants.js'
+import { colorThemeVariants } from './variants/color_theme_variants.js'
 
 addons.register(ADDON_ID, (api) => {
 	addons.add(TOOL_ID, {
@@ -9,4 +10,8 @@ addons.register(ADDON_ID, (api) => {
 		title: 'Variants',
 		render: () => <VariantBar api={api} />
 	})
+})
+
+addons.setConfig({
+	variants: [colorThemeVariants]
 })
