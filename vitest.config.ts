a8e84418eff96f084d5a2cd4e8725762e1ffb79f
unional <homawong@gmail.com>
2025-11-16 13:39:58 -0800

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vitest/config'
 
-// https://vitejs.dev/config/
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
-
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
 	test: {
@@ -18,32 +12,10 @@ export default defineConfig({
 			]
 		},
 		workspace: [
-			'./libs/storybook/vitest.config.node.ts',
-			'./libs/storybook/vitest.config.browser.ts',
-			{
-				extends: true,
-				plugins: [
-					// The plugin will run tests for the stories defined in your Storybook config
-					// See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-					storybookTest({
-						configDir: path.join(dirname, '.storybook')
-					})
-				],
-				test: {
-					name: 'storybook',
-					browser: {
-						enabled: true,
-						headless: true,
-						provider: 'playwright',
-						instances: [
-							{
-								browser: 'chromium'
-							}
-						]
-					},
-					setupFiles: ['apps/app/.storybook/vitest.setup.ts']
-				}
-			}
+			'./libs/storybook/vitest.config.dark.ts',
+			'./libs/storybook/vitest.config.light.ts',
+			'./libs/storybook/vitest.config.theme.ts',
+			'./apps/app/vitest.config.ts'
 		]
 	}
 })
