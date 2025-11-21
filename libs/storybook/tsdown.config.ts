5179e6a59cb2087d94ac79416040773b6813144e
unional <homawong@gmail.com>
2025-11-21 00:27:15 -0800

    refactor: extract common tsdown config and add @storybook/icons to external dependencies

--- libs/storybook/tsdown.config.ts
+++ libs/storybook/tsdown.config.ts
@@ -1,33 +1,41 @@
-import { defineConfig } from 'tsdown'
-const external = [
-	'@storybook-community/storybook-dark-mode',
-	'@storybook/addon-docs',
-	'@storybook/addon-themes',
-	'@storybook/addon-vitest',
-	'@storybook/react-vite',
-	'react',
-	'react-dom',
-	'storybook',
-	'storybook-addon-tag-badges'
-]
+import { defineConfig, type UserConfig } from 'tsdown'
+
+const commonConfig: UserConfig = {
+	platform: 'browser',
+	target: 'esnext',
+	external: [
+		'@storybook-community/storybook-dark-mode',
+		'@storybook/addon-docs',
+		'@storybook/addon-themes',
+		'@storybook/addon-vitest',
+		'@storybook/icons',
+		'@storybook/react-vite',
+		'react',
+		'react-dom',
+		'storybook',
+		'storybook-addon-tag-badges'
+	]
+}
+
 export default defineConfig([
 	{
+		...commonConfig,
 		entry: ['./src/index.ts'],
 		outDir: './esm'
 	},
 	{
+		...commonConfig,
 		entry: ['./src/manager/index.ts'],
-		outDir: './esm/manager',
-		external
+		outDir: './esm/manager'
 	},
 	{
+		...commonConfig,
 		entry: ['./src/storybook-addon-tag-badges/index.ts'],
-		outDir: './esm/storybook-addon-tag-badges',
-		external
+		outDir: './esm/storybook-addon-tag-badges'
 	},
 	{
+		...commonConfig,
 		entry: ['./src/storybook-dark-mode/index.ts'],
-		outDir: './esm/storybook-dark-mode',
-		external
+		outDir: './esm/storybook-dark-mode'
 	}
 ])
