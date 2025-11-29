a9ea238d9c6f1db5bcd1eb67c9eb83bd55c3139d
unional <homawong@gmail.com>
2025-11-29 15:22:39 -0800

    chore(config): disable header max length rule in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
 		'subject-case': DISABLE,
+		'header-max-length': DISABLE,
 		'body-max-line-length': DISABLE,
 		'footer-max-line-length': DISABLE
 	}
