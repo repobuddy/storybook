d3a8ecd816267e53d0b45bc5bde974fd82bef2b2
unional <homawong@gmail.com>
2025-12-26 11:59:17 -0800

    chore(storybook): refactor preview configuration and import order

--- libs/storybook/.storybook/preview.tsx
+++ libs/storybook/.storybook/preview.tsx
@@ -1,43 +1,28 @@
-import { defineDocsParam } from '#repobuddy/storybook'
-import {
-	createDarkModeDocsContainer,
-	defineDarkModeParam,
-	withDarkMode
-} from '#repobuddy/storybook/storybook-dark-mode'
+import { createDarkModeDocsContainer, defineDarkModeParam } from '#repobuddy/storybook/storybook-dark-mode'
 import type { Preview } from '@storybook/react-vite'
 
+import '../reset.css'
 import '../tailwind.css'
 
 const preview: Preview = {
 	tags: ['snapshot'],
 	parameters: {
-		...defineDocsParam({
+		docs: {
 			codePanel: true,
 			container: createDarkModeDocsContainer()
-		}),
-		...defineDarkModeParam({
-			current: 'dark',
-			stylePreview: true
-		}),
+		},
 		options: {
 			storySort: {
 				method: 'alphabetical',
-				order: ['decorators', 'parameters', 'manager', 'testing', '*']
+				order: ['components', 'decorators', 'parameters', 'manager', 'testing', '*']
 			}
-		}
-	},
-	decorators: [
-		withDarkMode({
-			bodyClass: 'text-black bg-white dark:text-white dark:bg-black'
+		},
+		...defineDarkModeParam({
+			current: 'dark',
+			stylePreview: true,
+			darkClass: ['dark', 'text-white', 'bg-black']
 		})
-		// withThemeByClassName({
-		// 	themes: {
-		// 		light: 'light',
-		// 		dark: 'dark'
-		// 	},
-		// 	defaultTheme: 'light'
-		// })
-	]
+	}
 }
 
 export default preview
