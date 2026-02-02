e949b3cf0f54b9b8e8b5a8302d3752d4949d182e
unional <unional@users.noreply.github.com>
2026-02-02 08:31:00 +0000

    deploy: 92a370077ac74625d8874f3d08d931aa29383089

---
+++ assets/types.meta.stories-BcxFBZSW.js
@@ -0,0 +1,28 @@
+import{j as o}from"./iframe-Bp8gbjS8.js";import{s as t}from"./with_story_card-CLs9kVTl.js";import{d as a}from"./define_docs_param-lmgaBKCF.js";import{d as r}from"./dedent-BuYMbVyj.js";import"./preload-helper-PPVm8Dsz.js";const c={title:"storybook-addon-tag-badges/types/Meta",tags:["new","version:1.0"],decorators:[t()],render:()=>o.jsx(o.Fragment,{})},e={name:"TypicalUsage",parameters:{...a({source:{code:r`
+                // component.stories.tsx
+                import type { Meta } from '@repobuddy/storybook/storybook-addon-tag-badges'
+
+                const meta: Meta<typeof Component> = {
+                    // ...
+                }
+
+                export default meta
+                `}})}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  name: 'TypicalUsage',
+  parameters: {
+    ...defineDocsParam({
+      source: {
+        code: dedent\`
+                // component.stories.tsx
+                import type { Meta } from '@repobuddy/storybook/storybook-addon-tag-badges'
+
+                const meta: Meta<typeof Component> = {
+                    // ...
+                }
+
+                export default meta
+                \`
+      }
+    })
+  }
+}`,...e.parameters?.docs?.source}}};const i=["TypicalUsage"];export{e as TypicalUsage,i as __namedExportsOrder,c as default};
