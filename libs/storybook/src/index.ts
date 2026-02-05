b1b5c5b6f1bdd4ec16d06005cdae307958c08526
unional <homawong@gmail.com>
2026-02-05 02:31:05 -0800

    feat: export story_card component from storybook index

--- libs/storybook/src/index.ts
+++ libs/storybook/src/index.ts
@@ -1,6 +1,7 @@
 export * from '@repobuddy/test'
 export type * from './arg-types/fn-to-arg-types.ts'
 export * from './components/show_html.tsx'
+export * from './components/story_card.tsx'
 export * from './decorators/show_doc_source.tsx'
 export * from './decorators/with_story_card.tsx'
 export * from './parameters/define_actions_param.ts'
