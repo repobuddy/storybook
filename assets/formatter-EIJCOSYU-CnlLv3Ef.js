4497f47919a5edc05029f200414efa6d61e00762
unional <unional@users.noreply.github.com>
2026-02-08 22:36:27 +0000

    deploy: 361617b7f26f85888414d51b3748309b67f2a969

--- assets/formatter-EIJCOSYU-CnlLv3Ef.js
+++
@@ -1,1 +0,0 @@
-import{c as i,e as a,f as m}from"./iframe-B9Ippj7-.js";import"./preload-helper-PPVm8Dsz.js";var t=a(m(),1),p=(0,t.default)(2)(async(r,e)=>r===!1?e:i(e));export{p as formatter};
---
+++ assets/snapshot_badge.stories-CFI18jL_.js
@@ -0,0 +1,7 @@
+import{j as s}from"./iframe-Ct7Ajxe4.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"storybook-addon-tag-badges/snapshotBadge",render:()=>s.jsx(s.Fragment,{})},t={tags:["snapshot","!test"],render:()=>s.jsxs("div",{children:[s.jsx("div",{children:"Story with snapshot"}),s.jsx("code",{children:"tags: ['snapshot']"})]})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  tags: ['snapshot', '!test'],
+  render: () => <div>
+            <div>Story with snapshot</div>
+            <code>tags: ['snapshot']</code>
+        </div>
+}`,...t.parameters?.docs?.source}}};const r=["SnapshotBadge"];export{t as SnapshotBadge,r as __namedExportsOrder,a as default};
