aafca7172c9636b1f48c13ff071deffb2d198b29
unional <unional@users.noreply.github.com>
2026-02-09 00:20:01 +0000

    deploy: fcee5b178eb96c91747317e5b480ba588b47d53b

--- assets/beta_badge.stories-GTcux4AQ.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./iframe-Brwb2-8P.js";import"./preload-helper-PPVm8Dsz.js";const o={title:"storybook-addon-tag-badges/betaBadge",render:()=>e.jsx(e.Fragment,{})},t={tags:["beta"],render:()=>e.jsxs("div",{children:[e.jsx("div",{children:"Warn that a component or prop is not stable yet"}),e.jsx("code",{children:"tags: ['beta']"})]})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  tags: ['beta'],
-  render: () => <div>
-            <div>Warn that a component or prop is not stable yet</div>
-            <code>tags: ['beta']</code>
-        </div>
-}`,...t.parameters?.docs?.source}}};const s=["BetaBadge"];export{t as BetaBadge,s as __namedExportsOrder,o as default};
