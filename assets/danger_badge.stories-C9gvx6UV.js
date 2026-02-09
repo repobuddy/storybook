7a650c1fc935fcaaa363adb44b9dc7b08b290d14
unional <unional@users.noreply.github.com>
2026-02-09 00:23:41 +0000

    deploy: ccc6f5e6848a5af7fdb93e0904839cf29a99388d

--- assets/danger_badge.stories-C9gvx6UV.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./iframe-xG9xzY9M.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"storybook-addon-tag-badges/dangerBadge",render:()=>e.jsx(e.Fragment,{})},r={tags:["danger"],render:()=>e.jsxs("div",{children:[e.jsx("div",{children:"Components that require particular attention when configuring them (e.g. for with security concerns)"}),e.jsx("code",{children:"tags: ['danger']"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  tags: ['danger'],
-  render: () => <div>
-            <div>Components that require particular attention when configuring them (e.g. for with security concerns)</div>
-            <code>tags: ['danger']</code>
-        </div>
-}`,...r.parameters?.docs?.source}}};const o=["DangerousBadge"];export{r as DangerousBadge,o as __namedExportsOrder,a as default};
---
+++ assets/new_badge-CiMW_60s.js
@@ -0,0 +1,10 @@
+import{j as e,M as r}from"./iframe-DX012i83.js";import{useMDXComponents as s}from"./index-BQs7DDjK.js";import"./preload-helper-PPVm8Dsz.js";function o(t){const n={code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",strong:"strong",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"storybook-addon-tag-badges/newBadge"}),`
+`,e.jsx(n.h1,{id:"new-badge",children:"New badge"}),`
+`,e.jsxs(n.p,{children:["Use the ",e.jsx(n.strong,{children:"new"}),' badge for recently added components or props/features. The badge displays 🆕 with a "New" tooltip.']}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsxs(n.p,{children:["Add the ",e.jsx(n.code,{children:"new"})," tag to your story."]}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-tsx",children:`export const YourStory = {
+	tags: ['new'],
+	render: () => <YourComponent />
+}
+`})})]})}function i(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{i as default};
