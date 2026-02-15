94fc19618d4abfff53c4703e1494d30bcd578847
unional <unional@users.noreply.github.com>
2026-02-15 00:20:58 +0000

    deploy: b713c3af3e68b740260138cc5fba7755bbee6f23

--- assets/version_badge.stories-n7_fYiBx.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./iframe-B9TY5VBC.js";import"./preload-helper-PPVm8Dsz.js";const a={title:"storybook-addon-tag-badges/versionBadge",tags:["var","version:2.4"],render:()=>e.jsx(e.Fragment,{})},r={tags:["version:1.0"],render:()=>e.jsxs("div",{children:[e.jsx("div",{children:"Version tag is used to indicate when the component/feature/property becomes available."}),e.jsx("code",{children:"tags: ['version:1.0']"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  tags: ['version:1.0'],
-  render: () => <div>
-            <div>Version tag is used to indicate when the component/feature/property becomes available.</div>
-            <code>tags: ['version:1.0']</code>
-        </div>
-}`,...r.parameters?.docs?.source}}};const n=["VersionBadge"];export{r as VersionBadge,n as __namedExportsOrder,a as default};
