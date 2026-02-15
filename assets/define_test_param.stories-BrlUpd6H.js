c1b6a99d5a22d30841470b80ab71794da7e30b75
unional <unional@users.noreply.github.com>
2026-02-15 00:35:29 +0000

    deploy: 5f63d09a455ad3d3e681edf5f5958643f3902d9b

---
+++ assets/define_test_param.stories-BrlUpd6H.js
@@ -0,0 +1,61 @@
+import{j as a}from"./iframe-t758EaAo.js";import{d as o}from"./dedent-BuYMbVyj.js";import{s as d}from"./show_doc_source-BJcZeenh.js";import{d as n}from"./define_docs_param-lmgaBKCF.js";import"./preload-helper-PPVm8Dsz.js";function t(c){return{test:c}}const f={title:"Parameters/defineTestParam",tags:["func","new","version:1.0"],parameters:{docs:{description:{component:"Helper function to define test parameters for Storybook stories"}}},decorators:[d()],render:()=>a.jsx(a.Fragment,{})},e={parameters:{...t({clearMocks:!0,mockReset:!0,restoreMocks:!0}),...n({source:{code:o`defineTestParam({
+                    clearMocks: true,
+                    mockReset: true,
+                    restoreMocks: true
+                })`}})}},r={parameters:{...t({clearMocks:!0,mockReset:!0,restoreMocks:!0,dangerouslyIgnoreUnhandledErrors:!0}),...n({source:{code:o`defineTestParam({
+                    clearMocks: true,
+                    mockReset: true,
+                    restoreMocks: true,
+                    dangerouslyIgnoreUnhandledErrors: true
+                })`}})}},s={parameters:{...t({clearMocks:!0}),...n({source:{code:o`defineTestParam({
+                    clearMocks: true
+                })`}})}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...defineTestParam({
+      clearMocks: true,
+      mockReset: true,
+      restoreMocks: true
+    }),
+    ...defineDocsParam({
+      source: {
+        code: dedent\`defineTestParam({
+                    clearMocks: true,
+                    mockReset: true,
+                    restoreMocks: true
+                })\`
+      }
+    })
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...defineTestParam({
+      clearMocks: true,
+      mockReset: true,
+      restoreMocks: true,
+      dangerouslyIgnoreUnhandledErrors: true
+    }),
+    ...defineDocsParam({
+      source: {
+        code: dedent\`defineTestParam({
+                    clearMocks: true,
+                    mockReset: true,
+                    restoreMocks: true,
+                    dangerouslyIgnoreUnhandledErrors: true
+                })\`
+      }
+    })
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  parameters: {
+    ...defineTestParam({
+      clearMocks: true
+    }),
+    ...defineDocsParam({
+      source: {
+        code: dedent\`defineTestParam({
+                    clearMocks: true
+                })\`
+      }
+    })
+  }
+}`,...s.parameters?.docs?.source}}};const k=["Default","WithDangerouslyIgnoreUnhandledErrors","MinimalConfig"];export{e as Default,s as MinimalConfig,r as WithDangerouslyIgnoreUnhandledErrors,k as __namedExportsOrder,f as default};
--- assets/editor_badge.stories-DZbaygBa.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./iframe-Qpe9uMKj.js";import"./preload-helper-PPVm8Dsz.js";const o={title:"storybook-addon-tag-badges/editorBadge",tags:["var","version:0.6"],render:()=>e.jsx(e.Fragment,{})},r={tags:["editor"],render:()=>e.jsxs("div",{children:[e.jsx("div",{children:"Story with live editor"}),e.jsx("code",{children:"tags: ['editor']"})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  tags: ['editor'],
-  render: () => <div>
-            <div>Story with live editor</div>
-            <code>tags: ['editor']</code>
-        </div>
-}`,...r.parameters?.docs?.source}}};const s=["EditorBadge"];export{r as EditorBadge,s as __namedExportsOrder,o as default};
