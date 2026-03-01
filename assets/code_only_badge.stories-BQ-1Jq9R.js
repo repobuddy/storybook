c50774db0640ee034482945ff305815a8c0da2ef
unional <unional@users.noreply.github.com>
2026-03-01 07:10:42 +0000

    deploy: 508aafd54a18594fe292409989597494aa8bdde1

---
+++ assets/code_only_badge.stories-BQ-1Jq9R.js
@@ -0,0 +1,16 @@
+import{j as e}from"./iframe-CPpg16TU.js";import{d as n}from"./dedent-BuYMbVyj.js";import{s as r}from"./show_source-CRov7rlt.js";import{w as t}from"./with_story_card-Cp4orGqg.js";import"./preload-helper-PPVm8Dsz.js";const p={title:"storybook-addon-tag-badges/codeOnlyBadge",tags:["var","!snapshot","version:0.11"],render:()=>e.jsx(e.Fragment,{})},o={tags:["code-only"],decorators:[t({content:e.jsxs("p",{children:["Components that only exist in code, and not in design. In the sidebar it appears as ",e.jsx("code",{children:"📝"}),"."]})}),r({source:n`export const YourStory = {
+                tags: ['code-only'],
+                render: () => <YourComponent />
+            }`})]};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  tags: ['code-only'],
+  decorators: [withStoryCard({
+    content: <p>
+                    Components that only exist in code, and not in design. In the sidebar it appears as <code>📝</code>.
+                </p>
+  }), showSource({
+    source: dedent\`export const YourStory = {
+                tags: ['code-only'],
+                render: () => <YourComponent />
+            }\`
+  })]
+}`,...o.parameters?.docs?.source}}};const m=["CodeOnlyBadge"];export{o as CodeOnlyBadge,m as __namedExportsOrder,p as default};
---
+++ assets/show_doc_source-DH7autsT.js
@@ -0,0 +1,1 @@
+import{s as r}from"./show_source-CRov7rlt.js";function s(o){return r({placement:"after",...o})}export{s};
