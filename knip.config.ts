0377e10709a4f3a1792a0bd929182ccb2f301117
unional <homawong@gmail.com>
2026-04-12 16:53:30 -0700

    chore: integrate knip into check scripts and update knip config

--- knip.config.ts
+++ knip.config.ts
@@ -4,17 +4,11 @@ const config: KnipConfig = {
 	workspaces: {
 		'libs/*': {
 			entry: ['**/index.{ts,tsx,js,jsx}', '**/src/index.{ts,tsx,js,jsx}', '**/.storybook/**/*.{ts,tsx,js,jsx}'],
-			project: ['**/*.{ts,tsx,js,jsx}'],
-			ignoreDependencies: [
-				// somehow it detects as unused
-				'tailwindcss',
-				// They are referenced in subpath usage.
-				'@storybook-community/storybook-dark-mode',
-				'storybook-addon-tag-badges'
-			]
+			project: ['**/*.{ts,tsx,js,jsx}']
 		}
 	},
-	ignore: ['apps/**']
+	ignore: ['apps/**'],
+	ignoreDependencies: ['@types/react']
 }
 
 export default config
