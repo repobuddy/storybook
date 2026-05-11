31ea5b21561da9f10579a978e7aa88839bc92865
unional <homawong@gmail.com>
2026-05-10 17:38:56 -0700

    refactor(storybook): replace sortStoryWithTag with createStorySort
    Replaces the `sortStoryWithTag` function with `createStorySort` to support Storybook 10+'s requirement for inline `storySort` functions. The new API returns a comparator function that can be delegated from the inline `storySort` definition in preview configs. Also adds comprehensive tests and a new import alias for `#storybook-addon-tag-badges`.

---
+++ .changeset/remove-sort-story-with-tag.md
@@ -0,0 +1,5 @@
+---
+"@repobuddy/storybook": minor
+---
+
+Add `sortStoryWithTag` for sorting stories by title hierarchy and tag priority.
--- libs/storybook/.storybook/preview.tsx
+++ libs/storybook/.storybook/preview.tsx
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react-vite'
 import { setupMonaco } from 'storybook-addon-code-editor'
-import { sortStoryWithTag } from '#repobuddy/storybook'
+import { createStorySort } from '#repobuddy/storybook'
 import { defineDarkModeParam } from '#repobuddy/storybook/storybook-dark-mode'
 import { onMonacoLoad } from './code-editor'
 
@@ -8,27 +8,31 @@ import './tailwind.css'
 
 setupMonaco({ onMonacoLoad })
 
+const compare = createStorySort({
+	order: [
+		'Overview',
+		'Changelog',
+		'components',
+		'decorators',
+		'parameters',
+		'arg-types',
+		'types',
+		'manager',
+		'testing',
+		'*'
+	],
+	tag: ['playground', 'use-case', 'example', 'spec', 'props', 'edge-case', 'unit']
+})
+
 const preview: Preview = {
 	parameters: {
 		docs: {
 			codePanel: true
 		},
 		options: {
-			storySort: sortStoryWithTag({
-				order: [
-					'Overview',
-					'Changelog',
-					'components',
-					'decorators',
-					'parameters',
-					'arg-types',
-					'types',
-					'manager',
-					'testing',
-					'*'
-				],
-				tag: ['playground', 'use-case', 'example', 'spec', 'props', 'edge-case', 'unit']
-			})
+			storySort(a: Parameters<typeof compare>[0], b: Parameters<typeof compare>[1]) {
+				return compare(a, b)
+			}
 		},
 		...defineDarkModeParam({
 			current: 'dark',
